a7a52a1131bf36fe60c6baeb3a11fdb80cf35025
iamfelix8 <iamfelix8@gmail.com>
2020-04-21 09:54:57 +0530

    Add files via upload
    This is a separate production build, in which unwanted files are removed and reduced the file size with only needed code.

---
+++ build/precache-manifest.5da19ee4deaf955b079961ee4543b243.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120c85a97db53f1ec931b61b0001dd90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cfa477d53102c2aac3b",
+    "url": "/main.css"
+  },
+  {
+    "revision": "5cfa477d53102c2aac3b",
+    "url": "/main.js"
+  }
+]);
